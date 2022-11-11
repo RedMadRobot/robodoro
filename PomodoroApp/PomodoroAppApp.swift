1947d4c5e9bde1b901968b563234c903db827b2c
Petr Tartynskikh <haiden_tenno@mail.ru>
2022-11-11 18:54:28 +0300

    Initial Commit

---
+++ PomodoroApp/PomodoroAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroAppApp.swift
+//  PomodoroApp
+//
+//  Created by Петр Тартынских  on 11.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
