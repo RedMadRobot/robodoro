b8e8cc26decd48e1625c2a5432d3821030552467
Petr Tartynskikh <haiden_tenno@mail.ru>
2022-12-15 00:40:30 +0300

    Воспроизведение вибрации/звука в соответствии с настройками

--- PomodoroApp/Business Logic/Services/FeedbackService.swift
+++ PomodoroApp/Business Logic/Services/FeedbackService.swift
@@ -6,7 +6,6 @@
 //
 
 import AVFoundation
-import CoreHaptics
 
 // MARK: - FeedbackService
 
@@ -17,11 +16,37 @@ protocol FeedbackService {
 // MARK: - FeedbackServiceImpl
 
 final class FeedbackServiceImpl: FeedbackService {
-        
+
+    // MARK: - Constants
+    
+    private enum Constants {
+        static let alarmUrl = "/System/Library/Audio/UISounds/alarm.caf"
+        static let alarmId: SystemSoundID = 1304
+        static let vibrationId: SystemSoundID = 4095
+    }
+    
+    // MARK: - Private Properties
+    
+    private let userDefaultsStorage: UserDefaultsStorage
+    
+    // MARK: - Init
+    
+    init(userDefaultsStorage: UserDefaultsStorage) {
+        self.userDefaultsStorage = userDefaultsStorage
+    }
+    
     // MARK: - Public Methods
     
     func playTimerEndSignal() {
-        let systemSoundID: SystemSoundID = 1304
-        AudioServicesPlayAlertSound(systemSoundID)
+        if userDefaultsStorage.soundEnabled {
+            var alarmId = Constants.alarmId
+            let url = URL(fileURLWithPath: Constants.alarmUrl)
+            AudioServicesCreateSystemSoundID(url as CFURL, &alarmId)
+            AudioServicesPlaySystemSound(alarmId)
+        }
+        
+        if userDefaultsStorage.hapticEnabled {
+            AudioServicesPlaySystemSound(Constants.vibrationId)
+        }
     }
 }
